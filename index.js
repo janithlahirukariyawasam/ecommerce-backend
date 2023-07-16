37c81386914a5b46da4338ac7fe3f45b3d6df4fd
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2023-07-16 23:15:51 +0530

    implement db connection and add models

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//backend of Ecommerce site
+const express = require('express')
+const dotenv = require('dotenv').config()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const app = express()
+
+mongoose.connect(process.env.MONGO_URL,()=>console.log('MongoDb is connected'))
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+
+app.listen(process.env.PORT, ()=>console.log('Server has started'))
