1a5c4ead9f4612cc22e038e2f0e4d4916b1392c0
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2023-08-04 20:54:30 +0530

    add product controller

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+app.use("/product", productController);
+
 app.get('/',(req,res)=>{
     res.json("ecommerce backend")
 })
-
-
 
 app.listen(process.env.PORT, ()=>console.log('Server has started'))
