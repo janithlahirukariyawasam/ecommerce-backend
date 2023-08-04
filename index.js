de15fd8f431ab22ec36a3c6612079dcd9c936343
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2023-08-04 12:57:00 +0530

    fixed mongodb not connect issue

--- index.js
+++ index.js
@@ -4,12 +4,28 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
 
-mongoose.connect(process.env.MONGO_URL,()=>console.log('MongoDb is connected'))
+async function connectToDatabase() {
+    try {
+      await mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log('Connected to MongoDB successfully!');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error.message);
+    }
+  }
+  
+connectToDatabase();
 
 //middlewares
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    res.json("ecommerce backend")
+})
 
 
 
