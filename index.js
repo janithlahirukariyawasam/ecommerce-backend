5fd2a03569bdc1a91e4f5d9e2c8d1ef253286c82
ChinthakaLakshan <88089519+ChinthakaLakshan@users.noreply.github.com>
2023-08-05 20:25:42 +0530

    Add controllers

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const app = express()
 
 mongoose.connect(process.env.MONGO_URL,()=>console.log('MongoDb is connected'))
 
-//middlewares
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-
+app.use("/auth", authController);
+app.use('/upload', uploadController)
 
 app.listen(process.env.PORT, ()=>console.log('Server has started'))
