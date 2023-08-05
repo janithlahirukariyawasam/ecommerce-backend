0fc891854732ff3bbc1c3961e71fafc2d3368541
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2023-08-05 13:01:31 +0530

    add uploadController

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const dotenv = require('dotenv').config()
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
+const authController = require("./controllers/authController");
 const productController = require("./controllers/productController");
+const uploadController = require("./controllers/uploadController");
 
 //connect to mongodb database
 async function connectToDatabase() {
@@ -25,7 +27,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+app.use('/images', express.static('public/images'));
+app.use("/auth", authController);
 app.use("/product", productController);
+app.use('/upload', uploadController)
 
 app.get('/',(req,res)=>{
     res.json("ecommerce backend")
--- models/Product.js
+++ models/Product.js
@@ -1,32 +1,35 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const ProductSchema = new mongoose.Schema({
-    title:{
-        type:[String],
-        required:true,
-        unique:true,
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: [String],
+      required: true,
+      unique: true,
     },
-    desc:{
-        type:String,
-        required:true,
-        min:6,
+    desc: {
+      type: String,
+      required: true,
+      min: 6,
     },
-    firstImg:{
-        type:String,
-        required:true,
+    firstImg: {
+      type: String,
+      required: true,
     },
-    secondImg:{
-        type:String,
-        required:true,
+    secondImg: {
+      type: String,
+      required: true,
     },
-    price:{
-        type:Number,
-        required:true,
+    price: {
+      type: Number,
+      required: true,
     },
-    stars:{
-        type:Number,
-        default:3,
-    }
-},{timestamps:true})
+    stars: {
+      type: Number,
+      default: 3,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Product",ProductSchema)
+module.exports = mongoose.model("Product", ProductSchema);
